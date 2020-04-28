4e3075103ecab2718cdcd835decdac8c75ebcc98
Juan David Castellanos <jd.cast@outlook.com>
2020-04-28 21:57:04 +0200

    added react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import Designer from './components/Templates/Designer';
 import { GlobalStyles } from './styling/GlobalStyles';
 import { createBrowserHistory } from 'history';
 import './styling/typography';
+import { TypographyStyle, GoogleFont } from 'react-typography';
+import typography from './styling/typography';
 
 export const history = createBrowserHistory();
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <TypographyStyle typography={typography} />
+      <GoogleFont typography={typography} />
       <GlobalStyles />
       <Designer />
     </ThemeProvider>
--- src/styling/typography.ts
+++ src/styling/typography.ts
@@ -8,13 +8,11 @@ export const fonts = {
 
 const typography = new Typography({
   baseFontSize: '16px',
-  // baseLineHeight: 1.55,
-  // headerLineHeight: 1.4,
   headerFontFamily: [fonts.heading],
   bodyFontFamily: [fonts.body],
   ...stowLakeTheme,
 });
-// Hot reload typography in development.
+
 if (process.env.NODE_ENV !== 'production') {
   typography.injectStyles();
 }
